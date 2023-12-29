a456e48590e2a938cebd06bdbc1f0465d1acf67c
Karol Ciolczyk - Consultant <karol.ciolczyk@bentley.com>
2023-12-29 20:45:33 +0100

    DataSource added

--- src/main/java/com/example/rowersi/rest/TestRestController.java
+++ src/main/java/com/example/rowersi/rest/TestRestController.java
@@ -2,6 +2,9 @@ package com.example.rowersi.rest;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestRestController {
 	
+	private DataSource dataSource;
+	
+	public TestRestController(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@GetMapping("/")
 	public String hello() {
 		return "Hello World !!";
+	}
+	@GetMapping("/datasource")
+	public String datasource() throws SQLException {
+		return "Hello World !!" + this.dataSource.getConnection().getMetaData();
 	}
 	
 //	@GetMapping("/mysql")
