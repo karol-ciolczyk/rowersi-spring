91cae5e6a8af33134d7ac99aeb45204a26ad2978
Karol Ciolczyk - Consultant <karol.ciolczyk@bentley.com>
2023-12-29 17:39:18 +0100

    jdburl properies

--- src/main/java/com/example/rowersi/rest/TestRestController.java
+++ src/main/java/com/example/rowersi/rest/TestRestController.java
@@ -1,5 +1,8 @@
 package com.example.rowersi.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,16 @@ public class TestRestController {
 	@GetMapping("/")
 	public String hello() {
 		return "Hello World !!";
+	}
+	
+	@GetMapping("/mysql")
+	public String mysql() throws URISyntaxException {
+		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+	    String username = jdbUri.getUserInfo().split(":")[0];
+	    String password = jdbUri.getUserInfo().split(":")[1];
+	    String port = String.valueOf(jdbUri.getPort());
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+		return "username: " + username + ", password: " + password + " jdbUrl: " + jdbUrl;
 	}
 }
