680a97d0f502fa1ec05e78fe46d401be6cd49f1c
Karol Ciolczyk - Consultant <karol.ciolczyk@bentley.com>
2023-12-29 17:41:52 +0100

    database connection properties added to application.properties file

--- src/main/java/com/example/rowersi/rest/TestRestController.java
+++ src/main/java/com/example/rowersi/rest/TestRestController.java
@@ -14,14 +14,14 @@ public class TestRestController {
 		return "Hello World !!";
 	}
 	
-	@GetMapping("/mysql")
-	public String mysql() throws URISyntaxException {
-		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-
-	    String username = jdbUri.getUserInfo().split(":")[0];
-	    String password = jdbUri.getUserInfo().split(":")[1];
-	    String port = String.valueOf(jdbUri.getPort());
-	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-		return "username: " + username + ", password: " + password + " jdbUrl: " + jdbUrl;
-	}
+//	@GetMapping("/mysql")
+//	public String mysql() throws URISyntaxException {
+//		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+//
+//	    String username = jdbUri.getUserInfo().split(":")[0];
+//	    String password = jdbUri.getUserInfo().split(":")[1];
+//	    String port = String.valueOf(jdbUri.getPort());
+//	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+//		return "username: " + username + ", password: " + password + " jdbUrl: " + jdbUrl;
+//	}
 }
